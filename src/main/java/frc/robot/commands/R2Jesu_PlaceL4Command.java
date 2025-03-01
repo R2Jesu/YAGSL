866849301bd54ff3a1677982b53bb9c871d6bdd7
Livs508 <26sampso@corjesu.org>
2025-03-01 12:06:08 -0600

    new auto and subsyem

--- src/main/java/frc/robot/commands/R2Jesu_PlaceL4Command.java
+++
@@ -1,5 +0,0 @@
-package frc.robot.commands;
-
-public class R2Jesu_PlaceL4Command {
-    
-}
---
+++ src/main/java/frc/robot/commands/pathplanner/R2Jesu_PlaceL4Command.java
@@ -0,0 +1,73 @@
+package frc.robot.commands.pathplanner;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.subsystems.PathEventSubsytem;
+import frc.robot.subsystems.swervedrive.SwerveSubsystem;
+
+
+public class R2Jesu_PlaceL4Command extends Command {
+
+    private final PathEventSubsytem m_Subsytem;
+
+    public R2Jesu_PlaceL4Command(PathEventSubsytem subsytem)
+    {
+        m_Subsytem = subsytem;
+    }
+    
+     /**
+   * The initial subroutine of a command.  Called once when the command is initially scheduled.
+   */
+  @Override
+  public void initialize()
+  {
+
+  }
+
+  /**
+   * The main body of a command.  Called repeatedly while the command is scheduled. (That is, it is called repeatedly
+   * until {@link #isFinished()}) returns true.)
+   */
+  @Override
+  public void execute()
+  {
+    m_Subsytem.placeL4();
+  }
+
+  /**
+   * <p>
+   * Returns whether this command has finished. Once a command finishes -- indicated by this method returning true --
+   * the scheduler will call its {@link #end(boolean)} method.
+   * </p><p>
+   * Returning false will result in the command never ending automatically. It may still be cancelled manually or
+   * interrupted by another command. Hard coding this command to always return true will result in the command executing
+   * once and finishing immediately. It is recommended to use *
+   * {@link edu.wpi.first.wpilibj2.command.InstantCommand InstantCommand} for such an operation.
+   * </p>
+   *
+   * @return whether this command has finished.
+   */
+  @Override
+  public boolean isFinished()
+  {
+    return false;
+  }
+
+  /**
+   * The action to take when the command ends. Called when either the command finishes normally -- that is it is called
+   * when {@link #isFinished()} returns true -- or when  it is interrupted/canceled. This is where you may want to wrap
+   * up loose ends, like shutting off a motor that was being used in the command.
+   *
+   * @param interrupted whether the command was interrupted/canceled
+   */
+  @Override
+  public void end(boolean interrupted)
+  {
+    
+  }
+}
+
